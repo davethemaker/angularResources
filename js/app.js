2d5f592fd65109ba7828c5f9076d0a11dca1f062
David Eliason <davethemaker@gmail.com>
2015-07-10 15:30:05 -0700

    Add ui-router and routes pointing to home, books, persons, websites partials

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-var angularResources = angular.module('angularResources', []);
+var angularResources = angular.module('angularResources', ['ui.router']);
+
+angularResources.config(function($stateProvider){
+	$stateProvider.state('home', {
+		url: "",
+		templateUrl: "partials/home.html"
+	});
+
+	$stateProvider.state('books', {
+		url:"/books",
+		templateUrl:"partials/books.html",
+		controller: 'DigitalCtrl'
+	});
+
+	$stateProvider.state('persons', {
+		url: "/persons",
+		templateUrl:"partials/persons.html",
+		controller: 'PeopleCtrl'
+	});
+
+	$stateProvider.state('websites', {
+		url: "/websites",
+		templateUrl:"partials/websites.html",
+		controller: 'DigitalCtrl'
+	});
+});
