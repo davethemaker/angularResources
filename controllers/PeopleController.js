e8ad60a612178877643ab38bed865a054b289c95
David <david.eliason@metaltoad.com>
2015-07-09 18:20:15 -0700

    add functionality for adding a new person

--- controllers/PeopleController.js
+++ controllers/PeopleController.js
@@ -1,7 +1,15 @@
 angularResources.controller('PeopleCtrl', function PeopleCtrl($scope){
+	$scope.box = ["one","two"];
 	$scope.persons = [
 		{ name: "Tom Schmitz"},
 		{ name: "Judy Schmitz"},
 		{ name: "Jill Schmitz"}
-	]
+	];
+	$scope.addPerson = function(){
+		$scope.persons.push(
+		{
+			name: $scope.personName
+		});
+		$scope.personName = "";
+	};
 });
